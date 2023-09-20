62f3f0b235c140eecd0a8aba360c8550eeb4b417
NNQA <anhla12h@gmail.com>
2023-09-21 01:03:25 +0700

    feat(cache): add cacheManager

--- src/main/java/com/example/springproject/security/service/CachingConfig.java
+++ src/main/java/com/example/springproject/security/service/CachingConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+import java.util.concurrent.TimeUnit;
+
 @Configurable
 @EnableCaching
 public class CachingConfig {
@@ -13,11 +15,9 @@ public class CachingConfig {
     @Bean
     public CacheManager cacheManager() {
         SpringCache2kCacheManager cacheManager = new SpringCache2kCacheManager();
-        if (cacheManager.getCacheNames().stream()
-                .filter(name -> name.equals("orderCaching"))
-                .count() == 0) {
+        if (cacheManager.getCacheNames().stream().noneMatch(name -> name.equals("orderCaching"))) {
             cacheManager.addCaches(
-                    b -> b.name("orderCaching")
+                    b -> b.name("orderCaching").expireAfterWrite(30, TimeUnit.DAYS)
             );
         }
         return cacheManager.getNativeCacheManager();
